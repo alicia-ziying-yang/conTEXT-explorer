cba08e5c431c31fab7bcc4c84cfdb1cee7b50878
Ziying Alicia Yang <81403454+alicia-ziying-yang@users.noreply.github.com>
2021-07-26 19:25:45 +1000

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -4,4 +4,5 @@ from dash.testing.application_runners import import_app
 def test_one(dash_duo):
 	app = import_app("app")
 	dash_duo.start_server(app)
-	dash_duo.wait_for_text_to_equal("#corpus-select-title", "Use an existing corpus", timeout=4)
+	dash_duo.wait_for_text_to_equal("#corpus-select-title", "Use an existing corpus", timeout=5)
+	
